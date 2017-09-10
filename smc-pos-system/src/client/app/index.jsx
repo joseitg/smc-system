4ec18e6cea7e23d1ca50226f2e76e9be1a790d5c
rfig081 <reynold0801@outlook.com>
2017-09-09 20:23:32 -0400

    Main Window

--- smc-pos-system/src/client/app/index.jsx
+++ smc-pos-system/src/client/app/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+const ReactRouter = require('react-router-dom')
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
 
+import HeaderFacturacion from './HeaderFacturacion.jsx';
+import WrapperFacturacion from './WrapperFacturacion.jsx';
 
 class App extends React.Component {
-    render()
-    {
-        return(
-            <div>
-                <p>Wubba Lubba Dub Dub!!</p>
-            </div>
-        )
+    render() {
+        return (
+            <Router>
+                <div className='window'>
+                    <HeaderFacturacion />
+                    <WrapperFacturacion />
+                </div>
+            </Router>
+        );
     }
 }
 
