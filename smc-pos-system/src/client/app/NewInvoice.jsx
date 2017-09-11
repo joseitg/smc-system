f9ce4d4a4ed6c05801e01c5039709f346f0e925a
Ignacio <Itorgonz@hotmail.com>
2017-09-10 21:16:55 -0400

    fix

--- smc-pos-system/src/client/app/NewInvoice.jsx
+++ smc-pos-system/src/client/app/NewInvoice.jsx
@@ -14,20 +14,20 @@ class NewInvoice extends React.Component {
             <div className="invoice">
                 <p>wubba lubba dub dub!!</p>
                 <form>
-                    <label HTMLFor="numOrden">Numero de Orden</label><input type="number" name="numOrden" id="" /><br />
-                    <label HTMLFor="nombre"> Nombre</label><input type="text" name="nombreCliente" id="" />
-                    <label HTMLFor="apellidos">Apellidos</label><input type="text" name="apellidos" id="" /><br />
-                    <label HTMLFor="fecha">Fecha</label> <input type="date" name="fecha" id="" /><br />
-                    <label HTMLFor="empleado">Empleado</label> <input type="text" name="empleado" id="" /><br />
-                    <label HTMLFor="totalFac">Total Factura</label> <input type="number" name="totalFactura" id="" /><br />
-                    <label HTMLFor="pronto">Pronto</label> <input type="number" name="pronto" id="" /><br />
-                    <label HTMLFor="financiera">Financiera</label> <select name="financiera" id="">
+                    <label htmlFor="numOrden">Numero de Orden</label><input type="number" name="numOrden" id="" /><br />
+                    <label htmlFor="nombre"> Nombre</label><input type="text" name="nombreCliente" id="" />
+                    <label htmlFor="apellidos">Apellidos</label><input type="text" name="apellidos" id="" /><br />
+                    <label htmlFor="fecha">Fecha</label> <input type="date" name="fecha" id="" /><br />
+                    <label htmlFor="empleado">Empleado</label> <input type="text" name="empleado" id="" /><br />
+                    <label htmlFor="totalFac">Total Factura</label> <input type="number" name="totalFactura" id="" /><br />
+                    <label htmlFor="pronto">Pronto</label> <input type="number" name="pronto" id="" /><br />
+                    <label htmlFor="financiera">Financiera</label> <select name="financiera" id="">
                         <option value="muebleria">Muebleria</option>
                         <option value="coop">Cooperativa</option>
                     </select><br />
-                    <label HTMLFor="tipodeventa">Tipo de Venta</label> <input type="text" name="tipodeVenta" id="" /><br />
+                    <label htmlFor="tipodeventa">Tipo de Venta</label> <input type="text" name="tipodeVenta" id="" /><br />
                 </form>
-
+                    
             </div>
         );
     }
